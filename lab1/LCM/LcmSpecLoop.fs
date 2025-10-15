3700a27b818992d38226aeeea2992a4ab4879723
romboooo <412902@niuitmo.ru>
2025-10-15 15:23:02 -0700

    lcm logic

--- lab1/LCM/LcmSpecLoop.fs
+++ lab1/LCM/LcmSpecLoop.fs
@@ -7,3 +7,11 @@ let rec gcd (a: int64) (b: int64) =
 let lcm (a: int64) (b: int64) = 
     if a = 0L || b = 0L then 0L
     else abs (a * b) / gcd a b
+
+let smallestMultipleSpecLoop n =
+    let numbers = 
+        seq {
+            for i in 1L..n do
+                yield i
+        }
+    numbers |> Seq.fold lcm 1L
