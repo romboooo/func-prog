4083b3621f150cec21d07e8800a092503ccdd369
romboooo <412902@niuitmo.ru>
2025-10-14 20:50:43 -0700

    feat: lcm task

--- lab1/Program.fs
+++ lab1/Program.fs
@@ -2,6 +2,7 @@ open LcmTailRec
 open LcmRec
 open LcmModuleSolution
 open System.Diagnostics
+open LcmInfList
 
 let runPythonScript (scriptPath: string) : unit =
     Process.Start("python3", scriptPath).WaitForExit() |> ignore
@@ -12,9 +13,11 @@ let main argv =
     let multipleTailRecResult = smallestMultipleTailRec 20
     let multipleRecResult = smallestMultipleRec 20
     let LcmModuleSolutionResult = Solution.findSmallestMultiple 20 
+    let LcmInfListResult = smallestMultipleInfList 20
     printfn "НОК (хвостовая рекурсия): %d" multipleTailRecResult
     printfn "НОК (обычная рекурсия): %d" multipleRecResult
     printfn "НОК (модульная реализация + map): %d" LcmModuleSolutionResult
+    printfn "НОК (inf sec): %d" LcmInfListResult
     printfn "НОК (python): "
     runPythonScript "LCM/lcm.py" 
 
